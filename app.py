dfc6ca3a77ff9e991a52cf1af4d4463b323663e1
onkar005 <76241179+onkar005@users.noreply.github.com>
2021-07-16 15:35:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import difflib
 app = Flask(__name__)
 
 # Config MySQL
-app.config['MYSQL_HOST'] = 'postgres://vjirmwgrznsiiy:a052c76e5450c489a7a06dc34f8bab1da2a62b2c829504def4516b7ed77a30bc@ec2-3-234-85-177.compute-1.amazonaws.com:5432/dfg0n9rlacsget'
+app.config['MYSQL_HOST'] = 'ec2-3-234-85-177.compute-1.amazonaws.com'
 app.config['MYSQL_USER'] = 'vjirmwgrznsiiy'
 app.config['MYSQL_PASSWORD'] = 'a052c76e5450c489a7a06dc34f8bab1da2a62b2c829504def4516b7ed77a30bc'
 app.config['MYSQL_PORT'] =  5432
